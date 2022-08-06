232cf049e0b4d8ca0b14ddae5bdedd58f1e67787
Emre Kalkay <kalkayemre@gmail.com>
2022-08-07 01:29:49 +0300

    first commit

--- src/City.js
+++ src/City.js
@@ -8,10 +8,8 @@ function City({city}) {
   return (
     <div className='description'>
         <h2>{city.name}</h2>
-        <h3 className={city.main.temp<20 ? "cold" : "hot"}>{city.main.temp}°C</h3>
-        <div className={city.weather[0].description.includes("cloud") ? "cloudy" : city.weather[0].description.includes("rain") ? "rainy" : city.weather[0].description.includes("snow") ? "snowy" : "sunny"}>  
+        <h3 className={city.main.temp<20 ? "cold" : "hot"}>{city.main.temp}°C</h3> 
         <h1>{city.weather[0].description}</h1>
-        </div>
     </div>
   )
 }
